509167e99d158790b4c1b439178cdebb0dbd28fd
ahmed mustafa <greatos@outlook.com>
2024-02-24 08:58:23 +0300

    changes

--- api/n11_api.py
+++ api/n11_api.py
@@ -1,18 +1,60 @@
+import json
 import os
 import re
 import time
 import requests
 import lxml.etree as ET
 from dotenv import load_dotenv
+import xmltodict
 
 load_dotenv()
 API_KEY = os.getenv('N11_KEY')
 API_SECRET = os.getenv('N11_SECRET')
-url = "https://api.n11.com/ws/ProductService/"
+url = "https://api.n11.com/ws"
 
 # Authenticate with your appKey and appSecret
 headers = {"Content-Type": "text/xml; charset=utf-8"}
-payload = f"""
+
+# Function for parsing the XML response received from the N11 API
+
+
+def assign_vars(response, response_namespace, list_name):
+    """
+    The function `assign_vars` extracts product information and the number of pages from an XML
+    response.
+
+    :param response: The `response` parameter is expected to be the response object returned from an API
+    call. It should contain the XML response data
+    :return: two values: `products_list` and `products_total`.
+    """
+    # Access the response elements
+    raw_xml = response.text
+    main_list = ""
+    sub_list = ""
+
+    # `tree = ET.fromstring(raw_xml)` is creating an ElementTree object from the raw XML response
+    # received from the N11 API. The `fromstring()` function is a method of the `lxml.etree` module
+    # that parses the XML string and returns an ElementTree object. This object represents the XML
+    # structure and allows for easy navigation and manipulation of the XML data.
+    revised_response = (raw_xml.replace(f"""<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"><SOAP-ENV:Header/><SOAP-ENV:Body>""", "")).replace(f"""</SOAP-ENV:Body></SOAP-ENV:Envelope>""", "")
+    response_json = xmltodict.parse(revised_response)
+    
+    if list_name in response_json[f"ns3:{response_namespace}"]:
+        items_list = next(iter(response_json[f"ns3:{response_namespace}"][list_name].values()))
+        items_total = response_json[f"ns3:{response_namespace}"]['pagingData']['totalCount']
+           
+        return items_list, items_total
+  
+
+def get_n11_stock_data(url):
+    """
+    The function `get_n11_stock_data` makes a POST request to retrieve stock data from the N11 API and
+    returns a list of product IDs and their corresponding stock quantities.
+    :return: The function `get_n11_stock_data` returns a list of dictionaries containing the product ID
+    and stock quantity for each product found in the response.
+    """
+
+    payload = f"""
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sch="http://www.n11.com/ws/schemas">
     <soapenv:Header/>
     <soapenv:Body>
@@ -29,56 +71,7 @@ payload = f"""
     </soapenv:Body>
 </soapenv:Envelope>
 """
-api_call = requests.post(url, headers=headers, data=payload)
-
-# Function for parsing the XML response received from the N11 API
-
-
-def assign_vars(response):
-    """
-    The function `assign_vars` extracts product information and the number of pages from an XML
-    response.
-
-    :param response: The `response` parameter is expected to be the response object returned from an API
-    call. It should contain the XML response data
-    :return: two values: `products_list` and `products_pages`.
-    """
-    # Access the response elements
-    raw_xml = response.text
-
-    # `tree = ET.fromstring(raw_xml)` is creating an ElementTree object from the raw XML response
-    # received from the N11 API. The `fromstring()` function is a method of the `lxml.etree` module
-    # that parses the XML string and returns an ElementTree object. This object represents the XML
-    # structure and allows for easy navigation and manipulation of the XML data.
-    tree = ET.fromstring(raw_xml)
-
-    # The line `namespaces = {"ns3": "http://www.n11.com/ws/schemas"}` is creating a dictionary that
-    # maps a namespace prefix (`ns3`) to a namespace URI (`http://www.n11.com/ws/schemas`).
-    namespaces = {"ns3": "http://www.n11.com/ws/schemas"}
-
-    # The line `products_raw_response = tree.find(".//ns3:GetProductListResponse", namespaces)` is
-    # finding the element in the XML response that corresponds to the "GetProductListResponse" tag.
-    products_raw_response = tree.find(
-        ".//ns3:GetProductListResponse", namespaces)
-
-    # The code snippet `products_list = products_raw_response.find("products").findall("product")` is
-    # finding the element in the XML response that corresponds to the "products" tag and then finding
-    # all child elements with the tag "product". This will return a list of XML elements representing
-    # each product in the response.
-    products_list = products_raw_response.find("products").findall("product")
-    products_pages = products_raw_response.find(
-        "pagingData").find("pageCount").text
-
-    return products_list, products_pages
-
-
-def get_n11_stock_data():
-    """
-    The function `get_n11_stock_data` makes a POST request to retrieve stock data from the N11 API and
-    returns a list of product IDs and their corresponding stock quantities.
-    :return: The function `get_n11_stock_data` returns a list of dictionaries containing the product ID
-    and stock quantity for each product found in the response.
-    """
+    url = url+"/ProductService/"
 
     # This is used to send a SOAP request to the N11 API to retrieve a list of products.
     api_call = requests.post(url, headers=headers, data=payload)
@@ -86,11 +79,11 @@ def get_n11_stock_data():
 
     # Status code of 200 means that the request was successful and the server returned the expected response.
     if api_call.status_code == 200:
-        products_list, products_pages = assign_vars(api_call)
+        products_list, products_total = assign_vars(api_call, "GetProductListResponse", "products")
         raw_elements = []
 
         # Process all pages found
-        while current_page < int(products_pages):
+        while current_page < int(products_total):
             if products_list is not None:
                 # Process the product data
                 for product in products_list:
@@ -113,12 +106,115 @@ def get_n11_stock_data():
             )
             api_call_loop = requests.post(
                 url, headers=headers, data=payload_dump)
-            products_list, products_pages = assign_vars(api_call_loop)
+            products_list, products_total = assign_vars(api_call_loop, "GetProductListResponse", "product")
     else:
         print("Error:", api_call.text)
 
     print("N11 SOAP Request is Successful. Response:", api_call.reason)
     return raw_elements
+
+
+def get_n11_detailed_order_list(url):
+
+    payload = f"""
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sch="http://www.n11.com/ws/schemas">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <sch:DetailedOrderListRequest>
+            <auth>
+                <appKey>{API_KEY}</appKey>
+                <appSecret>{API_SECRET}</appSecret>
+            </auth>
+            <searchData>
+                <productId></productId>
+                <status>Completed</status>
+                <buyerName></buyerName>
+                <orderNumber></orderNumber>
+                <productSellerCode></productSellerCode>
+                <recipient></recipient>
+                <sameDayDelivery></sameDayDelivery>
+                <period>
+                    <startDate></startDate>
+                    <endDate></endDate>
+                </period>
+                <sortForUpdateDate>true</sortForUpdateDate>
+            </searchData>
+            <pagingData>
+                <currentPage>0</currentPage>
+                <pageSize>100</pageSize>
+            </pagingData>
+        </sch:DetailedOrderListRequest>
+    </soapenv:Body>
+</soapenv:Envelope>
+"""
+    orders_url = f"{url}/orderService/"
+
+    # This is used to send a SOAP request to the N11 API to retrieve a list of products.
+    api_call = requests.post(orders_url, headers=headers, data=payload)
+    current_page = 0
+
+    # Status code of 200 means that the request was successful and the server returned the expected response.
+    if api_call.status_code == 200:
+        orders_list, orders_total = assign_vars(api_call, "DetailedOrderListResponse", "orderList")
+        raw_elements = []
+
+        # Process all pages found
+        while current_page < int(orders_total):
+            if orders_list is not None:
+                # Process the product data
+                for order in orders_list:       
+                    item = {}
+                    val = order.items()
+                    for item_key, item_value in order.items():
+                        if isinstance(item_value, dict):
+                            for sub_key, sub_value in item_value.items():
+                                if isinstance(sub_value, dict): 
+                                    for low_key, low_value in sub_value.items():
+                                        if isinstance(low_value, dict):
+                                            data_val = (f"{low_key}_{inner_key}: {low_value[inner_key]}" for inner_key in low_value.keys())
+                                            item.update(data_val)
+                                        else:
+                                            item[f"{sub_key}_{low_key}"] = low_value 
+                                else:
+                                    item[f"{item_key}_{sub_key}"] = sub_value 
+                        else:
+                            item[item_key] = item_value
+
+                    raw_elements.append(item)
+            else:
+                print("No orders found in the response.")
+
+            current_page += 1
+            payload_dump = payload.replace(
+                f"<currentPage>0</currentPage>",
+                f"<currentPage>{str(current_page)}</currentPage>",
+            )
+            orders_list, orders_total = looper(orders_url, payload_dump, "DetailedOrderListResponse", "orderList")
+    else:
+        print("Error:", api_call.text)
+
+    print("N11 detailed orders list extraction is Successful. || Response:", api_call.reason)
+    return raw_elements
+
+def looper(link, payload_dump, namespace, list_name):
+    while True:
+        api_call_loop = requests.post(
+                link, headers=headers, data=payload_dump)
+        if re.search("success", api_call_loop.text):            
+            orders_list, orders_total = assign_vars(api_call_loop, namespace, list_name)
+
+            return orders_list, orders_total
+        else:
+            time.sleep(1)
+
+get_n11_detailed_order_list(url)
+# get_n11_stock_data(url)
+
+def save_to_csv(data):
+    keys = set()
+    for item in data:
+        if isinstance(item, "dict"):
+            keys.update(item.keys())
 
 
 def post_n11_data(data):
